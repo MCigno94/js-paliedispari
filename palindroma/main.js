f421781a5e4c54f38fd59aa7502a8d9b048e9bbb
MCigno94 <mattiacigno27@gmail.com>
2022-04-02 15:41:21 +0200

    adjustment

--- palindroma/main.js
+++ palindroma/main.js
@@ -7,17 +7,15 @@ Creare una funzione per capire se la parola inserita è palindroma
 
 
 function palindroma(word) {
-    let text = '';
     let backwardsText = '';
 
     for (let i = 0; i < word.length; i++) {
-        text += word[i]
         backwardsText += word[word.length - i - 1];
     }
-    console.log(`Parola letta da sx verso dx: ${text}`);
+    console.log(`Parola letta da sx verso dx: ${word}`);
     console.log(`Parola letta al contrario: ${backwardsText}`);
 
-    if (text == backwardsText) {
+    if (word == backwardsText) {
         console.log(`La parola "${word}" è palindroma`);
     } else {
         console.log(`La parola "${word}" non è palindroma`);
